f5c2ba6a3695b5feb34d94994c25436fbe1615ea
Josh Frye <josh.n.frye@gmail.com>
2022-08-28 10:50:19 -0700

    First pass at using boto3

--- photo_loader.py
+++ photo_loader.py
@@ -1,2 +1,11 @@
+import boto3
+
+
+def main():
+    s3 = boto3.resource("s3")
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+
+
 if __name__ == "__main__":
-    print("hello world")
+    main()
