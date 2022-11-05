0133aaa7f91dfa4d9e99923b649d0612ae9c6fdf
Josh Frye <josh.n.frye@gmail.com>
2022-11-04 21:52:51 -0700

    #2 Send guess results to dynamo DB table instead of saving locally

--- random_page_server.py
+++ random_page_server.py
@@ -10,15 +10,20 @@ import photo_loader as pl
 
 
 PHOTOS_ROOT = pl.CACHED_PHOTOS_ROOT
-DYNAMODB_TABLE_NAME = "plant_flashcards-common_names"
+DYNAMODB_COMMON_NAMES_TABLE = "plant_flashcards-common_names"
+DYNAMODB_GUESS_RESULTS_TABLE = "plant_flashcards-guess_results"
 
 
 def main():
+    client = boto3.client("dynamodb")
+
     taxon = get_taxon()
-    common_name = get_common_name(taxon[1], taxon[2])
+    common_name = get_common_name(client, taxon[1], taxon[2])
+
     open_taxon_page(taxon, common_name)
+
     taxon_results = get_all_inputs()
-    save_results(taxon, taxon_results)
+    save_results(client, taxon, taxon_results)
 
 
 def get_taxon():
@@ -47,12 +52,11 @@ def open_taxon_page(taxon, common_name):
     webbrowser.open(os.path.abspath(PHOTOS_ROOT + "/flashcard.html"))
 
 
-def get_common_name(genus, species):
+def get_common_name(client, genus, species):
     """
     The common names are kept in a DynamoDB table; this retrieves it given a genus and species.
     """
-    client = boto3.client("dynamodb")
-    response = client.get_item(TableName=DYNAMODB_TABLE_NAME, Key={"Binomial": {'S': f"{genus} {species}"}})
+    response = client.get_item(TableName=DYNAMODB_COMMON_NAMES_TABLE, Key={"Binomial": {'S': f"{genus} {species}"}})
 
     # The data from the item in the table is always in the 'Item' sub-dictionary
     # That sub-dictionary has the attributes as keys ('Common name' here), and values that are sub-dictionaries
@@ -71,13 +75,25 @@ def get_all_inputs():
 
 def get_input(taxon_level):
     result = input("Type '1' if you guessed " + taxon_level + " correctly, '0' otherwise.\n")
-    result = "0" if result == "" else result
+    result = False if (result == "" or result == "0") else True
     return result
 
 
-def save_results(taxon, taxon_results):
-    with open("../flashcard_guess_results.csv", 'a') as f:
-        f.write(f"{int(time.time())},{','.join(taxon)},{','.join(taxon_results)}\n")
+def save_results(client, taxon, taxon_results):
+    response = client.put_item(
+        Item={
+            'Binomial': {'S': f'{taxon[1]} {taxon[2]}'},
+            'UnixTime-ns': {'N': str(time.time_ns())},
+            'Correct-Family': {'BOOL': taxon_results[0]},
+            'Correct-Genus': {'BOOL': taxon_results[1]},
+            'Correct-Species': {'BOOL': taxon_results[2]},
+            'Correct-CommonName': {'BOOL': taxon_results[3]},
+        },
+        TableName=DYNAMODB_GUESS_RESULTS_TABLE,
+    )
+
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        print(f"ERROR! Could not save guess results! Response: {response}")
 
 
 if __name__ == "__main__":
