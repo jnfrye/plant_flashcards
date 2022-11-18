27e94d8665468397f787c8f30a6ea3e8aaed2375
Josh Frye <josh.n.frye@gmail.com>
2022-11-18 13:04:49 -0800

    #3 First pass of retrieving and using guess results from DynamoDB table

--- random_page_server.py
+++ random_page_server.py
@@ -12,10 +12,38 @@ import photo_loader as pl
 PHOTOS_ROOT = pl.CACHED_PHOTOS_ROOT
 DYNAMODB_COMMON_NAMES_TABLE = "plant_flashcards-common_names"
 DYNAMODB_GUESS_RESULTS_TABLE = "plant_flashcards-guess_results"
+SECONDS_PER_DAY = 86400
+NS_PER_SECOND = 1E9
+MAX_GUESS_AGE_DAYS = 30
+
+
+class GuessResult:
+    def __init__(self, binomial, unix_time_ns, family_correct, genus_correct, species_correct, common_name_correct):
+        self.binomial = binomial
+        self.unix_time_ns = unix_time_ns
+        self.family_correct = family_correct
+        self.genus_correct = genus_correct
+        self.species_correct = species_correct
+        self.common_name_correct = common_name_correct
+
+    @staticmethod
+    def from_response_item(item):
+        result = GuessResult(
+            binomial=item['Binomial']['S'],
+            unix_time_ns=int(item['UnixTime-ns']['N']),
+            family_correct=item['Correct-Family']['BOOL'],
+            genus_correct=item['Correct-Genus']['BOOL'],
+            species_correct=item['Correct-Species']['BOOL'],
+            common_name_correct=item['Correct-CommonName']['BOOL'],
+        )
+
+        return result
 
 
 def main():
     client = boto3.client("dynamodb")
+
+    guess_results = get_recent_guesses(client)
 
     taxon = get_taxon()
     common_name = get_common_name(client, taxon[1], taxon[2])
@@ -24,6 +52,27 @@ def main():
 
     taxon_results = get_all_inputs()
     save_results(client, taxon, taxon_results)
+
+
+def get_recent_guesses(client):
+    min_guess_unix_time_ns = time.time_ns() - SECONDS_PER_DAY * NS_PER_SECOND * MAX_GUESS_AGE_DAYS
+    response = client.scan(
+        TableName=DYNAMODB_GUESS_RESULTS_TABLE,
+        IndexName="UnixTime-ns-index",
+        FilterExpression="#t >= :minTime",
+        ExpressionAttributeNames={"#t": "UnixTime-ns"},
+        ExpressionAttributeValues={":minTime": {'N': str(min_guess_unix_time_ns)}}
+    )
+
+    if "LastEvaluatedKey" in response:
+        raise Exception("NEED TO DEAL WITH LastEvaluatedKey")
+
+    guess_results = []
+    for item in response["Items"]:
+        guess_results.append(GuessResult.from_response_item(item))
+        print(guess_results[-1].binomial)
+
+    return guess_results
 
 
 def get_taxon():
@@ -37,7 +86,7 @@ def get_random_taxon():
 
 def open_taxon_page(taxon, common_name):
     pl.ensure_photos_cached(taxon)
-    
+
     photo_files = os.listdir(PHOTOS_ROOT + "/" + "/".join(taxon))
     photos = ["/".join(taxon) + "/" + x for x in photo_files]
 
@@ -45,10 +94,10 @@ def open_taxon_page(taxon, common_name):
     template = env.get_template("template.html")
     flashcard_html = template.render(
         photos=photos, family=taxon[0], genus=taxon[1], species=taxon[2], common_name=common_name)
-    
+
     with open(PHOTOS_ROOT + "/flashcard.html", 'w') as f:
         f.write(flashcard_html)
-    
+
     webbrowser.open(os.path.abspath(PHOTOS_ROOT + "/flashcard.html"))
 
 
@@ -69,7 +118,7 @@ def get_all_inputs():
     genus_input = get_input("GENUS")
     species_input = get_input("SPECIES")
     common_name_input = get_input("COMMON NAME")
-    
+
     return family_input, genus_input, species_input, common_name_input
 
 
