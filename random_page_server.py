09aa623baa30b5169695d1c7d539ae1cb9db88bd
Josh Frye <josh.n.frye@gmail.com>
2022-12-18 14:38:47 -0800

    #3 Retry taxa based on number of correct guesses since most recent incorrect guess

--- random_page_server.py
+++ random_page_server.py
@@ -15,6 +15,8 @@ DYNAMODB_GUESS_RESULTS_TABLE = "plant_flashcards-guess_results"
 SECONDS_PER_DAY = 86400
 NS_PER_SECOND = 1E9
 MAX_GUESS_AGE_DAYS = 30
+RETRY_DELAY_SECONDS = 60
+RETRY_FACTOR = 4  # If a retry is correctly guessed, this is how many times the prev retry delay until another retry
 
 
 class GuessResult:
@@ -39,13 +41,14 @@ class GuessResult:
 
         return result
 
+    def is_correct(self):
+        return self.family_correct and self.genus_correct and (self.species_correct or self.common_name_correct)
+
 
 def main():
     client = boto3.client("dynamodb")
 
-    guess_results = get_recent_guesses(client)
-
-    taxon = get_taxon()
+    taxon = get_taxon(client)
     common_name = get_common_name(client, taxon[1], taxon[2])
 
     open_taxon_page(taxon, common_name)
@@ -70,18 +73,69 @@ def get_recent_guesses(client):
     guess_results = []
     for item in response["Items"]:
         guess_results.append(GuessResult.from_response_item(item))
-        print(guess_results[-1].binomial)
 
-    return guess_results
-
-
-def get_taxon():
-    return get_random_taxon()
+    return sorted(guess_results, key=lambda x: x.unix_time_ns)
 
 
-def get_random_taxon():
+def get_taxon(client):
+    """
+    Determine the taxon that should be guessed on.
+    """
     all_taxons = pl.get_all_taxons()
+
+    binomial = get_binomial_to_retry(client)
+
+    if binomial is not None:
+        return get_taxon_from_binomial(all_taxons, binomial)
+
     return random.choice(all_taxons)
+
+
+def get_taxon_from_binomial(all_taxons, binomial):
+    genus, species = binomial.split()
+    for taxon in all_taxons:
+        if taxon[1] == genus and taxon[2] == species:
+            return taxon
+
+    raise Exception(f"Could not find taxon for binomial {binomial}!")
+
+
+def get_binomial_to_retry(client):
+    # Note that the code below assumes the guess results list is sorted chronologically
+    guess_results = get_recent_guesses(client)
+
+    # We determine, for each recently guessed on binomial, when it should be retried.
+    # To do this, first we determine what delays to use and what the most recent guess time was
+    binomial_retry_delays = dict()
+    binomial_most_recent_guess_times = dict()
+
+    for x in guess_results:
+        binomial_most_recent_guess_times[x.binomial] = x.unix_time_ns
+
+        if not x.is_correct():
+            binomial_retry_delays[x.binomial] = RETRY_DELAY_SECONDS * NS_PER_SECOND
+        elif x.binomial in binomial_retry_delays:
+            binomial_retry_delays[x.binomial] *= RETRY_FACTOR
+
+    # If we came up with no delays, there must not have been any recent incorrect guesses.
+    if len(binomial_retry_delays) == 0:
+        return None
+
+    # Then we add the delays to the most recent guess times to get the time after which it should be retried
+    binomial_retry_times = dict()
+    # If we didn't calculate a delay, the binomial must not have any incorrect guesses, so we don't care about it
+    for binomial in binomial_retry_delays.keys():
+        binomial_retry_times[binomial] = binomial_most_recent_guess_times[binomial] + binomial_retry_delays[binomial]
+
+    # We return the binomial with the oldest retry time that is before the current time
+    # To do this we just sort the retry times dict chronologically and take the first element
+    oldest_retry_time_kvp = sorted(binomial_retry_times.items(), key=lambda kvp: kvp[1])[0]
+
+    # We only return the oldest retry if it is before the present time.
+    if oldest_retry_time_kvp[1] < time.time_ns():
+        return oldest_retry_time_kvp[0]
+
+    return None
 
 
 def open_taxon_page(taxon, common_name):
