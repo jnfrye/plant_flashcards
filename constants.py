3b9f1ec708a43685d06bf8495aca85ca9ac22756
Josh Frye <josh.n.frye@gmail.com>
2022-12-19 11:52:11 -0800

    Forgot to add new files

---
+++ constants.py
@@ -0,0 +1,6 @@
+SECONDS_PER_DAY = 86400
+NS_PER_SECOND = 1E9
+
+# Dynamo DB tables
+DYNAMODB_GUESS_RESULTS_TABLE = "plant_flashcards-guess_results"
+DYNAMODB_COMMON_NAMES_TABLE = "plant_flashcards-common_names"
